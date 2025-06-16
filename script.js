37b2878f111810d0dc92e20b804a10acb4fa201f
KronosCoder <atasit.saengsak@gmail.com>
2025-06-16 11:56:52 +0700

    update

--- script.js
+++ script.js
@@ -7,7 +7,10 @@ const backgroundVideo = document.getElementById('backgroundVideo')
 const songListContainer = document.getElementById('songListContainer')
 const homePage = document.getElementById('homePage')
 const playerPageContainer = document.getElementById('playerPageContainer')
-
+const backButton = document.getElementById('backButton')
+const musicPlayerHeader = document.getElementById('musicPlayerHeader')
+const groupsPlayer = document.querySelectorAll('.group-player')
+const currentProgressbar = document.getElementById('currentProgressbar')
 const currentPage = 'current-page'
 
 function renderSongList () {
@@ -38,28 +41,69 @@ function listInteraction () {
     songListItems.forEach((item , index) => {
         item.addEventListener('mouseenter' , (e) => {
             currentBackgroundVideo(e.target.dataset.id)
-            console.log(e.target.dataset.id)
             videoBgContainer.classList.add('list-hovering')
         })
         item.addEventListener('mouseleave' , () => {
             videoBgContainer.classList.remove('list-hovering')
         })
         item.addEventListener('click' , (e) => {
-            renderPlayerPage(e.target.dataset.id)
+            const listItemId = e.target.closest('.song-list').dataset.id
+            handlePlayerPage(listItemId)
         })
     })
 }
 
-function renderPlayerPage (songId) {
-    homePage.classList.remove(currentPage)
-    if (!playerPageContainer.classList.contains(currentPage)) {
-        playerPageContainer.classList.add(currentPage)
+function handlePlayerPage (songId) {
+    let isPause = false
+
+    const showMusicPlayer = (function () {
+        initialVideoBackground()
+        homePage.classList.remove(currentPage)
+        groupsPlayer.forEach((group , index) => {
+            if (!group.classList.contains(currentPage)) { 
+                group.classList.add(currentPage)
+                videoBgContainer.classList.add('player-active')
+            }
+        })
+        
+        const currentData = currentSongData(songId)
+        musicPlayerHeader.innerHTML = `
+            <img id="currentSongPicture" src="${currentData.albumArtUrl}" alt="${currentData.title}">
+            <div class="current-song-detail">
+                <h3 id="currentSongName">${currentData.title}</h3>
+                <p id="currentSongArtist">${currentData.artist}</p>
+            </div>
+        `
+    })()
+
+    function currentSongData (songId) {
+        const data = songs.find(s => s.id == songId)
+        return data
+    }
+
+    function handleProgressbar () {
+        const data = currentSongData() 
+        const songLength = backgroundVideo.length
+        if (!isPause) {
+            for (let i = 0; i < data.) {
+
+            }
+        }
     }
 }
 
+function backToHome () {
+    groupsPlayer.forEach((group , index) => {
+        if (group.classList.contains(currentPage)) { 
+            group.classList.remove(currentPage)
+            videoBgContainer.classList.remove('player-active')
+        }
+    })
+    homePage.classList.add(currentPage)
+}
 
 function currentBackgroundVideo (songId) {
-    initailVideoBackground()
+    initialVideoBackground()
     songs.forEach((song , index) => {
         if (song.id == songId) {
             backgroundVideo.setAttribute('src' , song.videoBgSrc)
@@ -67,9 +111,11 @@ function currentBackgroundVideo (songId) {
     })
 }
 
-function initailVideoBackground () {
+function initialVideoBackground () {
     backgroundVideo.currentTime = 0
 }
+
+backButton.addEventListener('click' , () => backToHome())
 
 function initailFunction () {
     console.log('Started ...')
